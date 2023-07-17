97c5f797a34f6e5092cd1fdac2c7acfbdb491e68
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-07-18 00:56:18 +0530

    Time: 72 ms (7.69%) | Memory: 42.2 MB (55.38%) - LeetSync

--- 2278-find-three-consecutive-integers-that-sum-to-a-given-number/find-three-consecutive-integers-that-sum-to-a-given-number.js
+++ 2278-find-three-consecutive-integers-that-sum-to-a-given-number/find-three-consecutive-integers-that-sum-to-a-given-number.js
@@ -3,12 +3,8 @@
  * @return {number[]}
  */
 var sumOfThree = function(num) {
-  const start = Math.floor(num / 3) - 1;
-  for (let i = start; i <= start + 2; i++) {
-    if (i + (i + 1) + (i + 2) === num) {
+  if(num%3!=0)
+   return [];
+  const i = Math.floor(num / 3) - 1;
       return [i, i + 1, i + 2];
-    }
-  }
-
-  return [];
 };
