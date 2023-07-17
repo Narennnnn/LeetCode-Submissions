69cf4995e792536dc129e543784f72b1d00220a7
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-07-17 10:14:51 +0530

    Time: 54 ms (81.38%) | Memory: 43.2 MB (13.04%) - LeetSync

---
+++ 2864-is-object-empty/is-object-empty.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Object | Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    const arr=Object.keys(obj);
+    if(arr.length==0)
+     return true;
+    else
+     return false;
+};
