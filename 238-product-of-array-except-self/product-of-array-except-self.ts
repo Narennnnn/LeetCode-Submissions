f7341baa22de53ac66a97c750ab199a6ecc2dada
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-08-05 22:14:47 +0530

    Time: 100 ms (82.62%) | Memory: 54.1 MB (68.51%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.ts
+++ 238-product-of-array-except-self/product-of-array-except-self.ts
@@ -1,23 +1,18 @@
 function productExceptSelf(nums: number[]): number[] {
-    let totalProduct = 1;
-    let zeroCount = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            zeroCount++;
-        } else {
-            totalProduct *= nums[i];
-        }
+    const leftProduct: number[] = [];
+    const rightProduct: number[] = [];
+    leftProduct.push(1);
+    const n = nums.length;
+    for (let i = 1; i < n; i++) {
+        leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
     }
-    const result: number[] = [];
-    for (let i = 0; i < nums.length; i++) {
-        if (zeroCount > 1) {
-            result.push(0);
-        } else if (zeroCount === 1) {
-            result.push(nums[i] === 0 ? totalProduct : 0);
-        } else {
-            result.push(totalProduct / nums[i]);
-        }
+    rightProduct[n - 1] = 1;
+    for (let i = n - 2; i >= 0; i--) {
+        rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
     }
-
-    return result;
+    const ans: number[] = [];
+    for (let i = 0; i < n; i++) {
+        ans[i] = leftProduct[i] * rightProduct[i];
+    }
+    return ans;
 }
