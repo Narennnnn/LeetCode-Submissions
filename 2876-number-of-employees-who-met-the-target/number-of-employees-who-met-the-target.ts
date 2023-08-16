00df38cc4ade0bb4ddc47adc88745b39c2be9fe5
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-08-16 23:18:24 +0530

    Time: 77 ms (5.08%) | Memory: 44.6 MB (55.84%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.ts
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.ts
@@ -1,5 +1,4 @@
 function numberOfEmployeesWhoMetTarget(hours: number[], target: number): number {
-  let ans=0
-  for(let i=0;i<hours.length;i++)if(hours[i]>=target)ans++
-  return ans;
-};
+  const ans = hours.filter((hoursWorked) => hoursWorked >= target);
+  return ans.length;
+}
