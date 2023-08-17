e53ef8ebf7a504d95704e068564802f0a47bc4c7
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-08-18 01:17:13 +0530

    Time: 65 ms (30.76%) | Memory: 44.2 MB (14.16%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.ts
@@ -0,0 +1,15 @@
+function lengthOfLastWord(s: string): number {
+    let newStr = s.trim();
+    let ctr = 0;
+
+    for (let i = newStr.length - 1; i >= 0; i--) {
+        if (newStr[i] === ' ') {
+            break;
+        } else {
+            ctr++;
+        }
+    }
+    
+    console.log("Trimmed String:", newStr); // Print the trimmed string
+    return ctr;
+}
