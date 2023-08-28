cf1194cc13aba766ab8f2e4d5d847858da812b45
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-08-28 23:02:58 +0530

    Time: 54 ms (75.17%) | Memory: 43.6 MB (80.65%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.ts
+++ 74-search-a-2d-matrix/search-a-2d-matrix.ts
@@ -1,11 +1,22 @@
-function searchMatrix(matrix: number[][], target: number): boolean {
-    for(let i=0;i<matrix.length;i++){
-      for(let j=0;j<matrix[0].length;j++){
-          if(matrix[i][j]==target)
-          {
-              return true;
-          }
-      }
+function searchMatrix(matrix, target) {
+    let m = matrix.length; // Number of rows
+    let n = matrix[0].length; // Number of columns
+    
+    let left = 0;
+    let right = m * n - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        let midVal = matrix[Math.floor(mid / n)][mid % n];
+        
+        if (midVal == target) {
+            return true;
+        } else if (midVal < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
+    
     return false;
-};
+}
