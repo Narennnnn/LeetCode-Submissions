0ccd58cb754142bee4374e9d112aa52c030b26b4
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-09-11 21:06:11 +0530

    Time: 27 ms (5.22%) | Memory: 13.7 MB (26.77%) - LeetSync

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        unordered_map<int, vector<int>> groups;
+        vector<vector<int>> result;
+        for (int i = 0; i < groupSizes.size(); ++i) {
+            groups[groupSizes[i]].push_back(i);
+            cout << "Added index " << i << " to group size " << groupSizes[i] << endl;
+        }
+
+        for (const auto entry : groups) {
+            int groupSize = entry.first;
+            const vector<int> indices = entry.second;
+
+            for (int i = 0; i < indices.size(); i += groupSize) {
+                vector<int> subgroup(indices.begin() + i, indices.begin() + i + groupSize);
+                result.push_back(subgroup);
+            }
+        }
+
+        return result;
+    }
+};
