141e2124de4eb0adc31963076caec8e0f3501059
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-09-17 17:44:52 +0530

    Time: 48 ms (80.87%), Space: 42.5 MB (15.12%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,9 +4,29 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]===target)
-          return i;
+    let l = 0, h = nums.length - 1;
+    
+    while (l <= h) {
+        let mid = l + Math.floor((h - l) / 2);
+        
+        if (nums[mid] === target) {
+            return mid;
+        }
+        
+        if (nums[l] <= nums[mid]) {
+            if (nums[l] <= target && target < nums[mid]) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[h]) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
     }
-   return -1;
+    
+    return -1;
 };
