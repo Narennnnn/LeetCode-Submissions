aa215ce46471bc6869e911bf0ff9205b1beaedcd
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-12-31 00:19:33 +0530

    Time: 31 ms (5.81%) | Memory: 13.8 MB (78.59%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        //vector<bool>row(m);
-        vector<bool>col(n);
-        for(int i=0;i<m;i++)
-         {
-             for(int j=0;j<n;j++)
-             {
-                 if(matrix[i][j]==0)
-                 {
-                   //  row[i]=true;
-                     col[j]=true;
-                     cout<<i<<"#"<<j<<endl;
-                 }
-             }
-         }
-         for(int i=0;i<m;i++)
-         {
-             bool contains_zero=false; 
-             for(int j=0;j<n;j++)
-             {
-                if(matrix[i][j]==0)
-                {
-                    contains_zero=true;
-                    cout<<i<<" "<<j<<endl;
-                    break;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<bool> zeroRows(rows, false);
+        vector<bool> zeroCols(cols, false);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows[i] = true;
+                    zeroCols[j] = true;
                 }
-             }
-             for(int j=0;j<n;j++)
-             {
-                 if(contains_zero||col[j])
-                 {
-                     matrix[i][j]=0;
-                 }
-             }
-         }
+            }
+        }
+
+        // Set entire row to 0 if there's any 0 in that row
+        for (int i = 0; i < rows; i++) {
+            if (zeroRows[i]) {
+                for (int j = 0; j < cols; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        // Set entire column to 0 if there's any 0 in that column
+        for (int j = 0; j < cols; j++) {
+            if (zeroCols[j]) {
+                for (int i = 0; i < rows; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 };
