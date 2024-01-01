0a21c7ab9197398d31a0797db18d92bc93939683
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2024-01-01 23:14:27 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (68.88%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -4,6 +4,7 @@ public:
         vector<int> resArr(rowIndex + 1, 1);
         for(int k = 1; k <= rowIndex; k++) {
             long long nextVal = (long long)(resArr[k - 1]) * (rowIndex - k + 1) / k;
+            //Binomial Coefficient Formula for mext coefficient when previous available
             resArr[k] = nextVal;
         }
         return resArr;
