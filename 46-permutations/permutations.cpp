79d13449eedf98d9611e0c4392d2a16ae5721313
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2024-01-03 20:24:32 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (64.89%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,22 +1,29 @@
 class Solution {
-public:
-    void allPerm(int index,vector<int>& nums,vector<vector<int>>&ans)
-    {
-        if(index==nums.size())
-        {
-            ans.push_back(nums);
+private:
+    void findPerm(vector<vector<int>>&res,vector<int>&ds,vector<int>& nums,int freq[],int n){
+        if(ds.size()==n){
+            res.push_back(ds);
             return;
         }
-        for(int i=index;i<nums.size();i++)
-        {
-            swap(nums[index],nums[i]);
-            allPerm(index+1,nums,ans);
-            swap(nums[index],nums[i]);
+        for(int i=0;i<n;i++){
+            if(freq[i]==0){
+                freq[i]=1;
+                ds.push_back(nums[i]);
+                findPerm(res,ds,nums,freq,n);
+                freq[i]=0;
+                ds.pop_back();
+            }
         }
     }
+public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
-        allPerm(0,nums,ans);
-        return ans;
+        vector<vector<int>>resultComb;
+        vector<int>ds;
+        int freq[nums.size()];
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+         freq[i]=0;
+        findPerm(resultComb,ds,nums,freq,n);
+        return resultComb;
     }
 };
