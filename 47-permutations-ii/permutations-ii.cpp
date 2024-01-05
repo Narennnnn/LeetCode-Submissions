decc0aa089a41937524c3f0b516a73f2074b46f5
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2024-01-05 19:33:42 +0530

    Time: 4 ms (83.45%) | Memory: 9 MB (80.04%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,23 +1,30 @@
 class Solution {
-    void allPerm(int index,vector<int>nums,vector<vector<int>>&ans)
-    {
-        if(index==nums.size())
-        {
-            ans.push_back(nums);
+private:
+    void findPerm(vector<vector<int>>& res, vector<int>& ds, vector<int>& nums, int n, vector<bool>& used) {
+        if (ds.size() == n) {
+            res.push_back(ds);
             return;
         }
-        for(int i=index;i<nums.size();i++)
-        {
-            if((i!=index) && (nums[i]==nums[index])){continue;}
-            swap(nums[index],nums[i]);
-            allPerm(index+1,nums,ans);
+        for (int i = 0; i < n; i++) {
+            if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])) {
+                continue;//skip over repeated elements
+            }
+            used[i] = true;
+            ds.push_back(nums[i]);
+            findPerm(res, ds, nums, n, used);
+            used[i] = false;
+            ds.pop_back();
         }
     }
+
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-        allPerm(0,nums,ans);
-        return ans;
+        vector<vector<int>> resultComb;
+        vector<int> ds;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<bool> used(n, false);
+        findPerm(resultComb, ds, nums, n, used);
+        return resultComb;
     }
 };
