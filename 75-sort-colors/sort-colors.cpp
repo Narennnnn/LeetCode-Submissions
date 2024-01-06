b8df2289657b22c41db289df088e8dca56043e73
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2024-01-06 18:55:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.85%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -4,9 +4,8 @@ public:
         int n=nums.size(),mid=0,low=0,high=n-1;
         while(mid<=high){
             if(nums[mid]==0){
-                int temp=nums[mid];
                 nums[mid]=nums[low];
-                nums[low]=temp;
+                nums[low]=0;
                 low++;
                 mid++;
             }else if(nums[mid]==2){
