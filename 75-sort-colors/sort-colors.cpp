8aa4bd39fa614a004ce77de8eb1858b37c84d126
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2024-01-06 18:54:28 +0530

    Time: 4 ms (33.07%) | Memory: 8.6 MB (37.85%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,57 +1,22 @@
-
 class Solution {
 public:
-    void merge(vector<int>& nums, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-        vector<int> leftArr(n1);
-        vector<int> rightArr(n2);
-
-        for (int i = 0; i < n1; i++) {
-            leftArr[i] = nums[left + i];
-        }
-        for (int j = 0; j < n2; j++) {
-            rightArr[j] = nums[mid + j + 1];
-        }
-
-        int i = 0;
-        int j = 0;
-        int k = left;
-
-        while (i < n1 && j < n2) {
-            if (leftArr[i] <= rightArr[j]) {
-                nums[k] = leftArr[i];
-                i++;
-            } else {
-                nums[k] = rightArr[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < n1) {
-            nums[k] = leftArr[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2) {
-            nums[k] = rightArr[j];
-            j++;
-            k++;
-        }
-    }
-
-    void mergeSort(vector<int>& nums, int start, int end) {
-        if (start >= end)
-            return;
-        int mid = start + (end - start) / 2;
-        mergeSort(nums, start, mid);
-        mergeSort(nums, mid + 1, end);
-        merge(nums, start, mid, end);
-    }
-
     void sortColors(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
+        int n=nums.size(),mid=0,low=0,high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                low++;
+                mid++;
+            }else if(nums[mid]==2){
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }else if(nums[mid]==1){
+                mid++;
+            }
+        }
     }
 };
