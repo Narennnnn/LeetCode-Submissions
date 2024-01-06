be427795961056a0a9d8b8e87b138295296c400c
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2024-01-06 18:38:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (15.69%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+
+class Solution {
+public:
+    void merge(vector<int>& nums, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        vector<int> leftArr(n1);
+        vector<int> rightArr(n2);
+
+        for (int i = 0; i < n1; i++) {
+            leftArr[i] = nums[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            rightArr[j] = nums[mid + j + 1];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                nums[k] = leftArr[i];
+                i++;
+            } else {
+                nums[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            nums[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            nums[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeSort(vector<int>& nums, int start, int end) {
+        if (start >= end)
+            return;
+        int mid = start + (end - start) / 2;
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid + 1, end);
+        merge(nums, start, mid, end);
+    }
+
+    void sortColors(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+    }
+};
